990a03670992e7b8bb5b3cee8042e7bab4460061
Lucas Santos <lucas201273@gmail.com>
2022-01-10 20:28:00 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../../assets/img/Vector.svg'
+import './styles.css'
+
+function Header () {
+  return (
+      <header>
+        <nav>
+            <div>
+               <h1> DSMovie</h1>
+               <a href="https://github.com/Luke0195">
+                 <img src={logo} alt="github-logo"/>
+                <span>/Luke0195</span>
+               </a>
+            </div>
+        </nav>
+      </header>
+  )
+}
+
+export default Header
