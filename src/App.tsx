744ab2ef43d85a88b10867b7ed5d1cff964ce8b7
Lucas Santos <lucas201273@gmail.com>
2022-01-10 20:56:14 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-
+import Dashboard from './pages/Dashboard'
+import Form from './pages/Form'
 function App () {
   return (
     <React.Fragment>
-      <Header/>
+      <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/form">
+              <Route path=":movieId" element={<Form/>}/>
+            </Route>
+          </Routes>
+      </BrowserRouter>
     </React.Fragment>
   )
 }
