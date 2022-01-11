c84b4a1d08509034150382889d1443cda0c91f88
Lucas Santos <lucas201273@gmail.com>
2022-01-10 21:14:12 -0300

    chore: add pagination component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Pagination from '../../components/Pagination'
 export default function Dashboard () {
   return (
-    <h1> Dashboard Page</h1>
+  <>
+    <Pagination/>
+  </>
   )
 }
